4f89781e993d87a2021e42a386fa7aa2bc4be9fa
hh937808-web <hh937808@gmail.com>
2026-02-02 10:47:02 +0700

    Initial commit - DATN

---
+++ src/main/java/com/example/datn_sd_29/DatnSd29Application.java
@@ -0,0 +1,13 @@
+package com.example.datn_sd_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnSd29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnSd29Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datn_sd_29/DatnSd29ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn_sd_29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnSd29ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
