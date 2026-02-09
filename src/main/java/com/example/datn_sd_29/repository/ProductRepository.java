6c3778755f6c43f96be0ddad6b81e5666613cdbe
AnBach123 <anbach4865@gmail.com>
2026-02-09 11:20:57 +0700

    xóa update trạng thái món ăn

--- src/main/java/com/example/datn_sd_29/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.datn_sd_29.repository;
-
-import com.example.datn_sd_29.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
